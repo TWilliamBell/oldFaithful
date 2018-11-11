1901bb0098499c58a78260767b19865fc6a232e9
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-11-11 14:59:55 -0500

    Updating graphics to output pdfs

--- oldFaithfulClustering.R
+++ oldFaithfulClustering.R
@@ -8,7 +8,11 @@ oldFaithful <- read.csv("./Data/geyserTime.csv")
 
 dbscanGeyser1 <- dbscan(oldFaithful, eps = 2, minPts = 30) ## We see that due to the major difference between the two dimensions, that it is difficult to get the two parts to cluster separately without excluding many of the points in each.
 
+pdf("./DataVis/dbscanAttemptedClustering.pdf")
+
 plot(oldFaithful, col = colours()[dbscanGeyser1$cluster + 51])
+
+dev.off()
 
 ## We can do better by transforming the data or weighting the dimensions differently, e.g.
 
@@ -18,18 +22,30 @@ oldFaithful$Wait.Time.Transformed.to.Hrs <- oldFaithful$Wait.Time.since.Previous
 
 dbscanGeyser <- dbscan(cbind(oldFaithful$Eruption.Length..min., oldFaithful$Wait.Time.Transformed.to.Hrs), eps = 0.5)
 
-plot(oldFaithful[ , c(1,3)], col = colours()[dbscanGeyser$cluster+60], main = "Clustering according to DBSCAN of Old Faithful")
+pdf("./DataVis/dbscanBasedClusteringForOldFaithful.pdf")
+
+plot(oldFaithful[ , c(1,3)], col = colours()[dbscanGeyser$cluster+60], main = "Clustering according to DBSCAN of Old Faithful", xlab = "Eruption Length (min)", ylab = "Wait Time since Previous Eruption (hrs)")
+
+dev.off()
 
 ## Much better.
 
 kmeansGeyser1 <- kmeans(oldFaithful[ , 1:2], centers = 2)
 
+pdf("./DataVis/kMeansAttempt.pdf")
+
 plot(oldFaithful[ , c(1,3)], col = colours()[kmeansGeyser1$cluster+60], xlab = "Eruption Length (min)", ylab = "Wait Time since Previous Eruption (hrs)", main = "Clustering according to k-Means of Old Faithful")
+
+dev.off()
 
 ## k-Means doesn't work so well with the untransformed data either.
 
 kmeansGeyser <- kmeans(oldFaithful[ , c(1,3)], centers = 2)
 
+pdf("./DataVis/kMeansOldFaithful.pdf")
+
 plot(oldFaithful[ , c(1,3)], col = colours()[kmeansGeyser$cluster+60], xlab = "Eruption Length (min)", ylab = "Wait Time since Previous Eruption (hrs)", main = "Clustering according to k-Means of Old Faithful")
+
+dev.off()
 
 ## With the transformed data it does give us what we're looking for.  k-Means has other failings, for instance it classifies all the eruption length outliers as belonging to the nearest group.
